fc89bf283a94f63b47c3aa1e70ed139d96881e6b
moehami <m.aljaaly@yahoo.co.uk>
2024-11-28 19:04:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack(config) {
+    config.resolve.alias['react'] = path.resolve(__dirname, 'node_modules', 'react');
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/index.html',
+      },
+    ];
+  },
+  output: 'export', // Add this line to ensure proper static exporting
 };
 
-module.exports = nextConfig;
+export default config;
