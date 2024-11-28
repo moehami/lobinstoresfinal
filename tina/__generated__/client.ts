13bce15affa8e84cf3e958317bdfa4fd4e97764f
moehami <m.aljaaly@yahoo.co.uk>
2024-11-28 17:56:31 +0200

    Tina15

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/moehami/lobinstoresfinal/tina/__generated__/.cache/1732806434151', url: 'https://content.tinajs.io/1.5/content/9a0c51a1-268e-4eff-9dbd-ffb90344151a/github/main', token: 'a7e93094fdaad14ea939eeefb4e3e5de1a1e9111', queries,  });
+export const client = createClient({ cacheDir: '/home/moehami/lobinstoresfinal/tina/__generated__/.cache/1732808858707', url: 'https://content.tinajs.io/1.5/content/9a0c51a1-268e-4eff-9dbd-ffb90344151a/github/main', token: 'a7e93094fdaad14ea939eeefb4e3e5de1a1e9111', queries,  });
 export default client;
   
